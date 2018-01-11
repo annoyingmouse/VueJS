2830f172acd1f9bce5000e9b84076ba08ee97ffd
Dominic Myers <annoyingmouse@gmail.com>
2018-01-11 09:01:58 +0000

    Implementing a log

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     "data": {
         "playerHealth": 100,
         "monsterHealth": 100,
-        "gameIsRunning": false
+        "gameIsRunning": false,
+        "turns": []
     },
     "methods": {
         startGame() {
@@ -12,7 +13,12 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            const damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                "isPlayer": true,
+                "text": `Player hits monster for ${damage}`
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -30,11 +36,16 @@ new Vue({
             this.monsterAttacks();
         },
         giveUp() {
-
+            this.gameIsRunning = false;
         },
         monsterAttacks(){
-            this.playerHealth -= this.calculateDamage(5, 12);
+            const damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                "isPlayer": false,
+                "text": `Player hits monster for ${damage}`
+            });
         },
         "calculateDamage": (min, max) => Math.max(Math.floor(Math.random() * max) + 1, min),
         checkWin() {
