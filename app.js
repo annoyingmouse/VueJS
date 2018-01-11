5b1f10898f12fe9b683b4914ea85b88faa3d4d1b
Dominic Myers <annoyingmouse@gmail.com>
2018-01-11 08:53:17 +0000

    Dealing with special attacks

--- app.js
+++ app.js
@@ -3,9 +3,56 @@ new Vue({
     "data": {
         "playerHealth": 100,
         "monsterHealth": 100,
-        "gameStarted": false
+        "gameIsRunning": false
     },
     "methods": {
+        startGame() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack() {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+        },
+        specialAttack() {
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+        },
+        heal() {
 
+        },
+        giveUp() {
+
+        },
+        monsterAttacks(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        "calculateDamage": (min, max) => Math.max(Math.floor(Math.random() * max) + 1, min),
+        checkWin() {
+            if (this.monsterHealth <= 0) {
+                if (confirm("You won! New Game?")) {
+                    this.startGame()
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm("You lost! New Game?")) {
+                    this.startGame()
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 });
