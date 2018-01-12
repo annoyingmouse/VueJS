8ea90e131abd525a142a9624b0eb370e5ba76c47
Dominic Myers <annoyingmouse@gmail.com>
2018-01-12 15:46:10 +0000

    VueJS Lifecycle

--- app.js
+++ app.js
@@ -1,76 +1,19 @@
-const vm1Data = {
-    "title": "The VueJS Instance",
-    "showParagraph": false
-};
-
-Vue.component("hello",{
-    "template": "<h1>Hello</h1>"
-});
-
-const vm1 = new Vue({
-    "data": vm1Data,
-    "methods": {
-        show() {
-            this.showParagraph = true;
-            this.updateTitle("The VueJS Instance (Updated)");
-            console.log(this.$refs);
-            this.$refs.myButton.innerText = "Test";
-        },
-        updateTitle(title) {
-            this.title = title;
-        }
-    },
-    "computed": {
-        lowercaseTitle() {
-            return this.title.toLowerCase();
-        }
-    },
-    "watch": {
-        title(value) {
-            alert(`Title changed, new value: ${value}`);
-        }
-    }
-});
-vm1.$mount("#app1"); // Mounting element in the DOM, useful if it's not there yet...
-
-console.log(vm1.$data === vm1Data);
-
-vm1.$refs.heading.innerText = "something else";
-
-// setTimeout(() => {
-//     vm1.title = "Changed by timer";
-//     vm1.show();
-// }, 3000);
-
-const vm2 = new Vue({
-    "el": "#app2",
+new Vue({
+    "el": "#app",
     "data": {
-        "title": "The second Instance"
+        "title": "The VueJS Instance"
     },
+    "beforeCreate": () => console.log("beforeCreate()"),
+    "created": () => console.log("created()"),
+    "beforeMount": () => console.log("beforeMount()"),
+    "mounted": () => console.log("mounted()"),
+    "beforeUpdate": () => console.log("beforeUpdate()"),
+    "updated": () => console.log("updated()"),
+    "beforeDestroy": () => console.log("beforeDestroy()"),
+    "destroyed": () => console.log("destroyed()"),
     "methods": {
-        "onChange": () => vm1.title = "Changed"
+        "destroy": function(){
+            this.$destroy();
+        }
     }
 });
-
-const vm3 = new Vue({
-    "el": ".hello",
-    "template": "<h1>Hello</h1>"
-});
-vm3.$mount();
-// document.getElementById("app3").appendChild(vm3.$el);
-
-
-
-setTimeout(() => {
-    const div = document.createElement("div");
-    div.setAttribute("id", "app4");
-    document.body.appendChild(div);
-}, 2000);
-
-const vm4 = new Vue({
-    "template": "<h1>Delayed</h1>"
-});
-vm4.$mount();
-
-setTimeout(() => document.getElementById("app4").appendChild(vm4.$el), 5000);
-
