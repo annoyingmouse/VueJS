c706451575ecbb1fe6c0c96d5061e449f23658f2
Dominic Myers <annoyingmouse@gmail.com>
2018-01-15 11:01:52 +0000

    Creating a Component

---
+++ src/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Server status: {{ status }}</h1>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        "data": () => ({"status": "Critical"}),
+        "methods": {
+            changeStatus(){
+                this.status = "Normal"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
