a5397ef5a4243853e9c3adbbffa5b8c1b1ee95dc
Dominic Myers <annoyingmouse@gmail.com>
2018-01-15 10:55:15 +0000

    The Root Component in the App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-    <h1>Hello World!</h1>
+    <h1>Server status: {{ status }}</h1>
 </template>
 
 <script>
     export default {
+        "data": function() {
+            return {
+                "status": "Critical"
+            };
+        }
     }
 </script>
 
