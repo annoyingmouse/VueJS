7cb27335c12856fd47f3461ddb560e22f102887a
Dominic Myers <annoyingmouse@gmail.com>
2018-01-15 14:06:01 +0000

    Using components

--- src/Home.vue
+++ src/Home.vue
@@ -1,18 +1,14 @@
 <template>
     <div>
-        <h1>Server status: {{ status }}</h1>
-        <hr>
-        <button @click="changeStatus">Change Status</button>
+        <app-server-status v-for="(server, i) in 5" :key="i"></app-server-status>
     </div>
 </template>
 
 <script>
+    import ServerStatus from "./ServerStatus.vue"
     export default {
-        "data": () => ({"status": "Critical"}),
-        "methods": {
-            changeStatus(){
-                this.status = "Normal"
-            }
+        components: {
+            "app-server-status": ServerStatus
         }
     }
 </script>
