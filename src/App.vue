4be427bb6b1217bbf378a52f46efbb422f00e546
Dominic Myers <annoyingmouse@gmail.com>
2018-01-23 10:44:44 +0000

    Proper source here now

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-
+               
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {Quote} from "./components/Quote";
+
     export default {
-        components:{
-            appQuote: Quote
-        }
+        
     }
 </script>
 
 <style>
-
 </style>
