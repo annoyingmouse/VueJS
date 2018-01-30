e48f844715e32c21178ab4d8a34e73ee3f2a0f74
Dominic Myers <annoyingmouse@gmail.com>
2018-01-30 09:09:07 +0000

    Getting there

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <dir class="row">
+        <app-quote v-for="quote in quotes">{{quote}}</app-quote>
+    </dir>
+</template>
+
+<script>
+    import Quote from "./Quote.vue"
+    export default {
+        "name": "quote-grid",
+        "props": ["quotes"],
+        components: {
+            "appQuote": Quote
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
