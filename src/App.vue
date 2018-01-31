51122474e74fb40d44bb54a2c93345e34ea8501a
Dominic Myers <annoyingmouse@gmail.com>
2018-01-31 13:34:47 +0000

    Checkboxes and arrays - tasty

--- src/App.vue
+++ src/App.vue
@@ -47,13 +47,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -108,7 +110,7 @@
                         <p style="white-space: pre;">Message: {{message}}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{item}}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -129,7 +131,8 @@
                     password:"",
                     age:"27"
                 },
-                message : "A new Text Area"
+                message: "A new Text Area",
+                sendMail: []
             }
         }
     }
