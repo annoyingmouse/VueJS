62f78419f1fa3d689a5e03b5de5ceebe03552385
Dominic Myers <annoyingmouse@gmail.com>
2018-02-02 13:08:55 +0000

    Custom Form Elements

--- src/App.vue
+++ src/App.vue
@@ -103,6 +103,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -130,7 +135,7 @@
                         </ul>
                         <p>Gender: {{gender}}</p>
                         <p>Priority: {{selectedPriority}}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -139,6 +144,8 @@
 </template>
 
 <script>
+    import Switch from "./Switch.vue";
+
     export default {
         data(){
             return {
@@ -151,8 +158,12 @@
                 sendMail: [],
                 gender: "",
                 selectedPriority: "High",
-                priorities: ["High", "Medium", "Low"]
+                priorities: ["High", "Medium", "Low"],
+                dataSwitch: true
             }
+        },
+        components:{
+            appSwitch: Switch
         }
     }
 </script>
