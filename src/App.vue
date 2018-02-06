66b601ced12e2536009b243afd82b6057bcadced
Dominic Myers <annoyingmouse@gmail.com>
2018-02-06 14:54:10 +0000

    My solution

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built-in Directives</h1>
-                <p v-text="'Some Text'"></p>
-                <p v-html="'<strong>Some Strong Text<strong>'"></p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight="'red'">Colour This</p>
-                <p v-highlight:background.dalayed="'red'">Or Colour This</p>
-                <p v-local-highlight:background.dalayed.blink="{mainColour: 'red', secondColour: 'green', delay: 500}">Colour This, Too</p>
+                <h1>Directives Exercise</h1>
+                <p v-once.click="'Hello World'">Hello World</p>
             </div>
         </div>
     </div>
@@ -22,34 +12,12 @@
 <script>
     export default {
         directives:{
-            "local-highlight":{
+            "once":{
                 bind(el, binding, vnode){
-                    let delay = 0;
-                    if(binding.modifiers["dalayed"]){
-                        delay = 3000;
-                    }
-                    if(binding.modifiers["blink"]){
-                        let mainColour = binding.value.mainColour;
-                        let secondColour = binding.value.secondColour;
-                        let currentColour = mainColour;
-                        setTimeout(() => {
-                            setInterval(() => {
-                                currentColour === secondColour ? currentColour = mainColour: currentColour = secondColour;
-                                if(binding.arg === "background"){
-                                    el.style.backgroundColor = currentColour;
-                                }else{
-                                    el.style.color = currentColour;
-                                }
-                            }, binding.value.delay);
-                        }, delay);
-                    } else {
-                        setTimeout(() => {
-                            if(binding.arg === "background"){
-                                el.style.backgroundColor = binding.value.mainColour;
-                            }else{
-                                el.style.color = binding.value.mainColour;
-                            }
-                        }, delay);
+                    if(binding.modifiers["click"]){
+                        el.addEventListener("click", function(){
+                            console.log(binding.value);
+                        });
                     }
                 }
             }
