6706575b76ee9a0922922f1b8f6040f2b86eb378
Dominic Myers <annoyingmouse@gmail.com>
2018-02-06 16:42:03 +0000

    Creating and Using Mixins

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,13 @@
 </template>
 
 <script>
-    import List from "./List.vue"
+    import List from "./List.vue";
+    import {fruitMixin} from "./FruitMixin.js";
     export default {
         data(){
             return {
                 text: "Hello There!",
-                fruits:["Apple", "Banana", "Mango", "Melon"],
-                filterText: ""
+
             }
         },
         filters:{
@@ -31,16 +31,10 @@
                 return value.toUpperCase();
             }
         },
-        computed:{
-            filteredFruits(){
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                });
-            }
-        },
         components:{
             appList: List
-        }
+        },
+        mixins:[fruitMixin]
     }
 </script>
 
--- src/List.vue
+++ src/List.vue
@@ -10,20 +10,9 @@
 </template>
 
 <script>
+    import {fruitMixin} from "./FruitMixin.js";
     export default {
-        data(){
-            return {
-                fruits:["Apple", "Banana", "Mango", "Melon"],
-                filterText: ""
-            }
-        },
-        computed:{
-            filteredFruits(){
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                });
-            }
-        }
+        mixins:[fruitMixin]
     }
 </script>
 
