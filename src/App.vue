ba34c44e1ec930ff9c88a39e3c1e000a632780a7
Dominic Myers <annoyingmouse@gmail.com>
2018-02-06 15:14:27 +0000

    Improving your App with Filters and Mixins

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <button v-customOn:click="clicked" class="btn btn-primary">Click me</button>
-                <hr>
-                <div
-                        v-custom-on:mouseenter="mouseEnter"
-                        v-custom-on:mouseleave="mouseLeave"
-                        style="width: 100px; height: 100px; background-color: lightgreen;"></div>
+                <h1>Filters & Mixins</h1>
+
             </div>
         </div>
     </div>
@@ -16,29 +11,6 @@
 
 <script>
     export default {
-        directives:{
-            "customOn":{
-                bind(el, binding){
-                    // el.onclick = function(){
-                    //     binding.value();
-                    // }
-                    const type = binding.arg;
-                    const fn = binding.value;
-                    el.addEventListener(type, fn);
-                }
-            }
-        },
-        methods:{
-            clicked(){
-                alert("I was clicked");
-            },
-            mouseEnter(){
-                console.log("Mouse entered")
-            },
-            mouseLeave(){
-                console.log("Mouse left")
-            }
-        }
     }
 </script>
 
