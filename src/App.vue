615c0f3fa5b0231b6b2113faafbc5cd748c618e3
Dominic Myers <annoyingmouse@gmail.com>
2018-02-07 15:53:30 +0000

    Fading transition

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br>
                 <br>
-                <transition>
+                <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
             </div>
@@ -26,5 +26,18 @@
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
 
+    }
+    .fade-leave {
+
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
 </style>
