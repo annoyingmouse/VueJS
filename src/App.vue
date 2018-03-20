dd59c41b974de40213f9e9778bfc82c7b41aad54
Dominic Myers <annoyingmouse@gmail.com>
2018-03-20 15:26:59 +0000

    HTTP

--- src/App.vue
+++ src/App.vue
@@ -2,76 +2,38 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">The Super Quiz</h1>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <transition name="flip" mode="out-in">
-                    <component
-                            :is="mode"
-                            @answered="answered($event)"
-                            @confirmed="mode='app-question'"></component>
-                </transition>
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label for="email">Mail</label>
+                    <input type="email" class="form-control" id="email" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
-    import Answer from './components/Answer.vue';
-
     export default {
         data() {
             return {
-                mode: 'app-question'
-            }
-        },
-        methods: {
-            answered(isCorrect) {
-                if (isCorrect) {
-                    this.mode = 'app-answer';
-                } else {
-                    this.mode = 'app-question';
-                    alert('Wrong, try again!');
+                user: {
+                    username:"",
+                    email:""
                 }
             }
         },
-        components: {
-            appQuestion: Question,
-            appAnswer: Answer
+        methods: {
+            submit(){
+                console.log(this.user);
+            }
         }
     }
 </script>
+
 <style>
-    .flip-enter{
-        /*transform: rotateY(0deg);*/
-    }
-    .flip-enter-active{
-        animation: flip-in 0.5s ease-out forwards;
-    }
-    .flip-leave{
-        /*transform: rotateY(0deg);*/
-    }
-    .flip-leave-active{
-        animation: flip-out 0.5s ease-out forwards;
-    }
-    @keyframes flip-out {
-        from{
-            transform: rotateY(0deg);
-        }
-        to{
-            transform: rotateY(90deg);
-        }
-    }
-    @keyframes flip-in {
-        from{
-            transform: rotateY(90deg);
-        }
-        to{
-            transform: rotateY(0deg);
-        }
-    }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -41,15 +41,15 @@ module.exports = {
     noInfo: true
   },
   devtool: '#eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: "production"
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
